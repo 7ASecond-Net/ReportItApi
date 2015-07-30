3b5ef20565de2b7c314f72f4ee9a858b13f26ae4
Dave Gordon <admin@7asecond.net>
2015-07-30 22:24:06 +0100

    Added ReportIt Model

---
+++ ReportIt/Controllers/ReportsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReportIt.Controllers
+{
+    public class ReportsController : ApiController
+    {
+      
+        // POST api/values
+        public bool Post([FromBody]string value)
+        {
+            try
+            {
+                string[] parts = value.Split(',');
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }       
+    }
+}
