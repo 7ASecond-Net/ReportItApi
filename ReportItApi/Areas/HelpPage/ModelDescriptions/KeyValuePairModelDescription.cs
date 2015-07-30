7a06bf235c718f7a233d4153b03df1c5486505e5
Dave Gordon <admin@7asecond.net>
2015-07-30 10:32:03 +0100

    Initial Commit

---
+++ ReportItApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ReportItApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ ReportItApi/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReportItApi.Controllers
+{
+    public class ReportController : ApiController
+    {
+        
+        // POST api/Report
+        public void Post([FromBody]string value)
+        {
+
+        }
+        
+    }
+}
